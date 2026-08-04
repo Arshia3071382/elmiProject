df44a153c1e5105e064662a8d74dfa5ad47de68a
Arshia <arshia.mokhtari99@gmail.com>
2026-08-05 01:49:58 +0330

    seniorAdminPAnel  section  is completing

--- src/component/HeroSec.tsx
+++ src/component/HeroSec.tsx
@@ -6,11 +6,10 @@ import Image from 'next/image'
 
 const images: string[] = [
   "/image/hero.png",
-  "/image/hero8.png",
-  "/image/hero3.png",
   "/image/hero10.png",
   "/image/hero6.png",
-  "/image/hero7.png",
+  "/image/hero11.png",
+  "/image/hero12.png",
   "/image/hero9.png"
 ]
 
