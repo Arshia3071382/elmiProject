2880f2590225f62f8aa2cf5eb16fc8200ee33c65
Arshia <arshia.mokhtari99@gmail.com>
2026-09-23 05:13:39 +0330

    story page in web app is completing

--- src/app/api/admin/live/[id]/route.ts
+++ src/app/api/admin/live/[id]/route.ts
@@ -3,10 +3,15 @@ import { NextResponse } from "next/server";
 import dbConnect from "./../../../../../../lib/dbConnect";
 import LiveStream from "./../../../../../../models/LiveStream";
 
-export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+export async function DELETE(
+  req: Request, 
+  { params }: { params: Promise<{ id: string }> }
+) {
   try {
     await dbConnect();
-    await LiveStream.findByIdAndDelete(params.id);
+    const { id } = await params;
+    
+    await LiveStream.findByIdAndDelete(id);
     return NextResponse.json({ success: true, message: "با موفقیت حذف شد" });
   } catch (error) {
     return NextResponse.json({ success: false, message: "خطا در حذف برنامه" }, { status: 500 });
